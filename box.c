55bbd3163bf293de70c72862917897365431d629
Safal <074bct531.safal@pcampus.edu.np>
2018-02-20 23:14:36 +0545

    Changed box.c

--- box.c
+++ box.c
@@ -10,72 +10,67 @@
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
  }
 
- void func(void);
+ void x_axis(int,int,int);
+ void y_axis(int,int,int);
+ void date(int, int);
+ void out(void);
+ int i;//global variable
+
+ void end(void);
 int main()
 {
-    int year,month,day,i;
+    int year,month,day;
     gotoxy(25,0);
-    printf("A.D TO B.S CONVERTER");
+    printf("DATE CONVERTER");
     gotoxy(14,2);
-    printf("Date in A.D.");
-    gotoxy(8,3);
-    for(i=0;i<25;i++){
-            gotoxy(8+i,3);
-            printf("%c",219);
-            gotoxy(8+i,11);
-            printf("%c",219);
-    }
-    gotoxy(7,4);
-    for(i=0; i<9;i++)
-    {
-        gotoxy(7,3+i);
-        printf("%c\n",219);
-        gotoxy(33,3+i);
-        printf("%c\n",219);
-    }
-    gotoxy(10,5);
-    printf("Year  ");
-    gotoxy(10,9);
-    printf("Day  ");
-    gotoxy(10, 7);
-    printf("Month  ");
+    printf("Date in B.S.");
+
+    y_axis(7,3,9);
+    y_axis(31,3,9);
+    x_axis(7,3,25);
+    x_axis(7,11,25);
+
+    date(10,5);
+    year_1:
+    gotoxy(18,5);
+    scanf("%d",&year);
+    if(year < 2000 || year > 2090)
+       {
+        out();
+        goto year_1;
+       }
+    month_1:
+    gotoxy(18,7);
+    scanf("%d",&month);
+    if(month < 1 || month >12)
+        {
+            out();
+            goto month_1;
+        }
+    day_1:
+    gotoxy(18,9);
+    scanf("%d",&day);
+    if(day < 1 || day > 32)
+        {
+            out();
+            goto day_1;
+        }
 
     gotoxy(45,2);
     printf("Date in B.S.");
-    gotoxy(41,3);
-     for(i=0;i<25;i++){
-            gotoxy(41+i,3);
-            printf("%c",219);
-            gotoxy(41+i,11);
-            printf("%c",219);
-    }
-    gotoxy(40,4);
-    for(i=0; i<9;i++)
-    {
-        gotoxy(40,3+i);
-        printf("%c\n",219);
-        gotoxy(66,3+i);
-        printf("%c\n",219);
-    }
-    gotoxy(43,5);
-    printf("Year  ");
-    gotoxy(43,9);
-    printf("Day  ");
-    gotoxy(43, 7);
-    printf("Month  ");
+    x_axis(41,3,25);
+    x_axis(41,13,25);
+    y_axis(41,3,11);
+    y_axis(65,3,11);
+    gotoxy(43,11);
+    printf("Weekday ");
+    date(43,5);
 
-    gotoxy(18,5);
-    scanf("%d",&year);
-    gotoxy(18,7);
-    scanf("%d",&month);
-    gotoxy(18,9);
-    scanf("%d",&day);
-
-    func();
+    end();
     return 0;
 }
 
-void func(void)
+void end(void)
 {
     int i;
     gotoxy(7,20);
@@ -100,3 +95,38 @@ void func(void)
 
     }
 }
+
+    void x_axis(int x, int y, int len)//for border along the x-axis
+    {//takes position of x, y and length of the box as arguments
+        gotoxy(x,y);
+        for(i=0;i<len;i++){
+            gotoxy(x+i,y);
+            printf("%c",219);
+    }
+    }
+
+    void y_axis(int x, int y, int len)//for border along the y-axis
+    {//takes position of x , y and breadth of the box as arguments
+        gotoxy(x,y);
+         for(i=0; i<len;i++)
+    {
+        gotoxy(x,y+i);
+        printf("%c\n",219);
+    }
+    }
+
+    void date(int x, int y)//displaying the date format
+    {//it takes the position of x and y as arguments
+        gotoxy(x,y);
+        printf("Year  ");
+        gotoxy(x,y+2);
+        printf("Month  ");
+        gotoxy(x, y+4);
+        printf("Day  ");
+        }
+
+    void out(void)
+    {
+        gotoxy(20,20);
+        printf("OUT OF RANGE!!!Please enter again...");
+    }
